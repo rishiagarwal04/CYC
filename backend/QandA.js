e41b4a9b8a49f6e481625fd78a93da223a0a2dec
vineet <Mastergenos228@gmail.com>
2025-06-26 18:51:15 +0530

    Production based configuration settings

--- backend/QandA.js
+++ backend/QandA.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Question = require("./QandA.js");
+const Question = require("./Model/QandA.js");
 
 // Post a question
 router.post("/", async (req, res) => {
